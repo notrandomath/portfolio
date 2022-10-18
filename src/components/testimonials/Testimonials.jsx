3e831e883b11052a30eb707b401e2b2b6f63981d
notrandomath <therealrandomath@gmail.com>
2022-10-18 12:47:15 -0700

    Added Navbar menu and UI icons

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,8 @@ import "./testimonials.scss"
 
 export default function Testimonials() {
   return (
-    <div className="testimonials">Testimonials</div>
+    <div className="testimonials" id = "testimonials">
+      Testimonials
+    </div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import "./topbar.scss"
+import {Phone, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
     <div className="topbar">
-        <h1>Welcome to Omar's Portfolio</h1>
+      <div className = "wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Omar's Portfolio Page.</a>
+          <div className="itemContainer">
+            <Phone className="icon"/>
+            <span>+1(909)682-9840</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>omar@seba3y.com</span>
+          </div>
+        </div>
+        <h1>Hello</h1>
+        <div className="right">
+          right 
+        </div>
+      </div>
     </div>
   )
 }
