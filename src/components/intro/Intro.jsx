3af73ef08b7d38095df8fd25e82de3a071f4c8ff
notrandomath <therealrandomath@gmail.com>
2022-10-26 16:30:53 -0700

    added animated typing and changed color scheme

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,9 @@
 import './intro.scss'
 import {KeyboardArrowDown} from "@material-ui/icons"
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Intro() {
+
   return (
     <div className="intro" id="intro">
         <div className="left">
@@ -13,7 +15,14 @@ export default function Intro() {
           <div className="wrapper">
             <h2>Hi There, I'm </h2>
             <h1>Omar Yahia</h1>
-            <h3>UCI<span></span></h3>
+            <h3>&gt; UCI&nbsp;
+            <Typewriter 
+              className="typing" 
+              words = {['Hobbyist Programmer', 'CS Student', 'Anteater']} 
+              loop = {0} //for infinite looping
+              cursor
+            />
+            </h3>
           </div>
           <a href="#portfolio">
             <KeyboardArrowDown className="icon"/>
